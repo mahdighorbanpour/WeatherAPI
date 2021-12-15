c36111e896a8b79afeafed1f033b1d2be1bb0909
mahdi.ghorbanpour <mahdi.ghorbanpour@gmail.com>
2021-12-15 09:18:30 +0100

    Created initial project structure

---
+++ test/MovingDots.MyWeather.Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovingDots.MyWeather.Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
